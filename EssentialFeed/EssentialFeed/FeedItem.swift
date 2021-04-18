87e2f4905a3099f6151f8195224b78b439afeefc
seabjackson <honeyboyseab@gmail.com>
2021-04-18 19:29:29 -0400

    Create essential Feed framework

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Seab Jackson on 4/18/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
