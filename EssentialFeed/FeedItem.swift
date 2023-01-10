6f27d04551920a6239f773b79790ed533f348207
Seab Jackson <honeyboyseab@gmail.com>
2023-01-10 13:22:00 -0500

    created project and added FeedItem

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Seab Jackson on 1/10/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let uuid: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
