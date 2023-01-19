c42b44159a9e70b32bfc30fa481298b9a83a0649
Seab Jackson <honeyboyseab@gmail.com>
2023-01-19 08:55:34 -0500

    moved 'RemoteFeedLoader' components to production

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,30 +6,13 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-       client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT()
+        
         XCTAssertNil(client.requestedURL)
     }
     
@@ -57,5 +40,4 @@ class RemoteFeedLoaderTests: XCTestCase {
             requestedURL = url
         }
     }
-
 }
