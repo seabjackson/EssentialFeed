7a5273d9c98688d0ad42103f3c15574414c8879e
Seab Jackson <honeyboyseab@gmail.com>
2023-02-16 09:19:19 -0500

    moved 'URLSessionHTTPClient' to its own file in production

---
+++ EssentialFeed/URLSessionHTTPClient.swift
@@ -0,0 +1,31 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Seab Jackson on 2/16/23.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { data, response, error in
+            if let error = error  {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }
+        .resume()
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+}
