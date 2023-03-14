a8304ea0d1b7b2d58758e5f015e281891c955ae5
Seab Jackson <honeyboyseab@gmail.com>
2023-03-14 05:48:24 -0400

    extracted test set up to a helper method

--- EssentialFeedAPIE2ETests/EssentialFeedAPIE2ETests.swift
+++ EssentialFeedAPIE2ETests/EssentialFeedAPIE2ETests.swift
@@ -11,19 +11,8 @@ import EssentialFeed
 final class EssentialFeedAPIE2ETests: XCTestCase {
     
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
         
-        let exp = expectation(description: "Wait for load completion")
-        var receivedResult: LoadFeedResult?
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 10.0 )
-        
-        switch receivedResult {
+        switch getFeedResult() {
         case let .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             XCTAssertEqual(items[0], expectedItem(at: 0))
@@ -46,6 +35,21 @@ final class EssentialFeedAPIE2ETests: XCTestCase {
     
     // MARK: - Helpers
     
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5c52cdd0b8a045df091d2fff/1548930512083/feed-case-study-test-api-feed.json")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        let exp = expectation(description: "Wait for load completion")
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 10.0 )
+        return receivedResult
+    }
+    
     private func expectedItem (at index: Int) -> FeedItem {
         return FeedItem(
             id: id(at: index),
@@ -55,16 +59,17 @@ final class EssentialFeedAPIE2ETests: XCTestCase {
     }
     
     private func id(at index: Int) -> UUID {
-        return UUID(uuidString: [
-            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
-            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-            "FFOECFE2-2879-403F-8DBE-A83B4010B340",
-            "DC97EF5E-2CC9-4905-AAD-3C351C311001",
-            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-            "F79BD7F8-063F-46E2-8147-A67635C3BB01"][index])!
-    }
+            return UUID(uuidString: [
+                "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+                "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+            ][index])!
+        }
     
     private func description(at index: Int) -> String? {
         return [
